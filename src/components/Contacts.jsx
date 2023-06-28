7d49f5c493cfd430dfc46fe993570df30bd3a8c8
Antonio Yustis <antonioyustis2020@gmail.com>
2023-06-28 12:38:06 -0400

    fixing contact animation

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -16,7 +16,7 @@ const Contacts = () => {
       <div
         ref={ref}
         style={{
-          transform: isInView ? 'none' : 'translateX(-200px)',
+          transform: isInView ? 'none' : 'translateX(+200px)',
           opacity: isInView ? 1 : 0,
           transition: 'all 0.6s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
         }}
